f70d2e531d5eb426293ccb06b89ce252863e7759
Sary Elmelegy <arch.selmelegy@gmail.com>
2018-10-13 13:23:47 +0200

    Adding showLimit function to hide specified list items

--- js/script.js
+++ js/script.js
@@ -5,12 +5,25 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const studentsList = document.querySelector(".student-list");
-// console.log(studentsList.children.length);
+const students     = studentsList.children;
+const studentsNum  = studentsList.children.length;
 
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+// Tip: Keep in mind that with a list of 54 students, the last page will only display four
+function showLimit(list, idx){
+    const firstStudentIndex = (idx - 1) * 10;
+    const lastStudentIndex  = firstStudentIndex + 9;
 
+    for (let i = 0; i < studentsNum; i++){
+        if (i >= firstStudentIndex && i <= lastStudentIndex){
+            students[i].style.display = 'block';
+        } else {
+            students[i].style.display = 'none';
+        }
+    }
+}
+// showLimit(students, 2);
 
 
 
