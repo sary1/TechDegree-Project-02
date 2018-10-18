3bf2d1e54bbe192dbb62a0b0aaab24c3db528aa5
Sary Elmelegy <arch.selmelegy@gmail.com>
2018-10-18 18:05:12 +0200

    Reorganization and Adding code comments

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
-/*// Global Variables
-const studentsList = document.querySelector(".student-list");
-const students     = studentsList.children;
-const studentsNum  = studentsList.children.length;
-const mainDiv      = document.querySelector('.page');
-const filterDiv    = document.createElement('div');
+const studentsList  = document.querySelector(".student-list");
+const students      = Array.from(studentsList.children);
+const studentsNum   = studentsList.children.length;
+let shownStudents;
+const mainDiv       = document.querySelector('.page');
+const filterDiv     = document.createElement('div');
 
 
-// a function that takes a list and a page index as inputs and filters list items to show only the students in that page
-function showLimit(list, idx){
+// a function that takes a list of students and page index as inputs and returns a new list of the ten students on that list
+function pageEdit(studentsList, pageIndex){
+    firstIndex = Math.ceil((pageIndex - 1) * 10);
+    lastIndex = firstIndex + 9;
+    newList = [];
 
-    // creating a range of students' names to show on a single page
-    const firstStudentIndex = (idx - 1) * 10;
-    const lastStudentIndex  = firstStudentIndex + 9;
-
-    // Loop over the whole students list and show only the ones on the given index
-    for (let i = 0; i < studentsNum; i++){
-        if (i >= firstStudentIndex && i <= lastStudentIndex){
-            students[i].style.display = 'block';
-        } else {
-            students[i].style.display = 'none';
+    // a loop over the main list and append the required indeces to the new initialized list
+    for(let i = firstIndex; i <= lastIndex; i++){
+        if(studentsList[i]){
+            newList.push(studentsList[i]);
         }
     }
+    return newList;
 }
 
 
+// a function that takes a list of students and a page index as inputs and show only the ten required students
+function showPage(studentsList, pageIndex){
+    shownStudents = pageEdit(studentsList, pageIndex);
+    students.forEach((student, index) => {
+        if(shownStudents.indexOf(student) != -1){
+            student.style.display = "block";
+        } else {
+            student.style.display = "none";
+        }
+    })
+}
 
-// a function that creates pagination links based on the number of students that is given as an input
-function paginationLinks(studentsNum){
-    const linksNum  = Math.ceil(studentsNum / 10);
+// a function that takes the number of students as an input and append to the DOM the pagination links needed
+function createPagination(studentsNum){
+    const pageNum = Math.ceil(studentsNum / 10);
+    const ul   = document.createElement('ul');
 
     // a loop that creates each pagination link and adds it to the DOM with the first link active
-    for(let i = 0; i < linksNum; i++){
+    for(let i = 0; i < pageNum; i++){
+
         const link = document.createElement('li');
         const aTag = document.createElement('a');
         aTag.href = '#';
@@ -39,20 +50,21 @@ function paginationLinks(studentsNum){
             aTag.classList.add('active');
         }
         link.appendChild(aTag);
-        filterDiv.appendChild(link);
+        ul.appendChild(link);
     }
 
+    filterDiv.appendChild(ul);
     filterDiv.classList.add('pagination');
     mainDiv.appendChild(filterDiv);
 }
 
 
-
 // Listenenig to click events on the pagination links
+let searchedList = Array.from(students);
+
 filterDiv.addEventListener('click', (e) => {
-    // e.preventDefault();
     const targetedLink = e.target;
-    const listItems    = filterDiv.children;
+    const listItems    = filterDiv.children[0].children;
     const index        = parseInt(targetedLink.textContent);
 
     // remove "active" class from all list anchor tags
@@ -60,17 +72,18 @@ filterDiv.addEventListener('click', (e) => {
         listItems[i].children[0].classList.remove('active');
     }
 
+    // adding active mood to the current page only
     targetedLink.classList.add('active');
-    showLimit(students, index);
-})
 
+    // showing the students that exist on the specific index the user wants
+    showPage(searchedList, index);
+})
 
 /*-----------------------------------------
 -------------------------------------------
 Search Bar addition and pagination response
 -------------------------------------------
 -----------------------------------------*/
-
 
 // Creating search bar
 const pageHeader  = document.querySelector('.page-header');
@@ -98,102 +111,9 @@ append(searchBar, [searchInput, searchBtn]);
 append(pageHeader, [searchBar]);
 
 
-/*
 // Adding search functionality
 searchBar.addEventListener('keyup', (e) => {
-    const search = searchInput.value.toLowerCase();
-
-    for(let student of students){
-        const studnetName =
-            student.querySelector('h3').textContent.toLowerCase();
-
-        if(studnetName.indexOf(search) == -1){
-            student.style.display = 'none';
-        } else {
-            student.style.display = 'block';
-        }
-    }
-})
-
-
-
-// Initializing the page with the first 10 students and the first pagination link in the active mode
-showLimit(students, 1);
-paginationLinks(studentsNum);
-*/
-const studentsList  = document.querySelector(".student-list");
-const students      = Array.from(studentsList.children);
-const studentsNum   = studentsList.children.length;
-let shownStudents;
-const mainDiv       = document.querySelector('.page');
-const filterDiv     = document.createElement('div');
-
-
-function pageEdit(studentsList, pageIndex){
-    firstIndex = Math.ceil((pageIndex - 1) * 10);
-    lastIndex = firstIndex + 9;
-    newList = [];
-
-    for(let i = firstIndex; i <= lastIndex; i++){
-        if(studentsList[i]){
-            newList.push(studentsList[i]);
-        }
-    }
-    return newList;
-}
-
-function showPage(studentsList, pageIndex){
-    shownStudents = pageEdit(studentsList, pageIndex);
-    students.forEach((student, index) => {
-        if(shownStudents.indexOf(student) != -1){
-            student.style.display = "block";
-        } else {
-            student.style.display = "none";
-        }
-    })
-}
-
-function createPagination(studentsNum){
-    const pageNum = Math.ceil(studentsNum / 10);
-    const ul   = document.createElement('ul');
-
-    // a loop that creates each pagination link and adds it to the DOM with the first link active
-    for(let i = 0; i < pageNum; i++){
-
-        const link = document.createElement('li');
-        const aTag = document.createElement('a');
-        aTag.href = '#';
-        aTag.textContent = i + 1;
-        if(i === 0){
-            aTag.classList.add('active');
-        }
-        link.appendChild(aTag);
-        ul.appendChild(link);
-    }
-
-    filterDiv.appendChild(ul);
-    filterDiv.classList.add('pagination');
-    mainDiv.appendChild(filterDiv);
-}
-
-let searchedList = Array.from(students);
-// Listenenig to click events on the pagination links
-filterDiv.addEventListener('click', (e) => {
-    const targetedLink = e.target;
-    const listItems    = filterDiv.children[0].children;
-    const index        = parseInt(targetedLink.textContent);
-
-    // remove "active" class from all list anchor tags
-    for(let i = 0; i < listItems.length; i++){
-        listItems[i].children[0].classList.remove('active');
-    }
-
-    targetedLink.classList.add('active');
-    showPage(searchedList, index);
-})
-
-// Adding search functionality
-searchBar.addEventListener('keyup', (e) => {
+    // Assure the search input is lower case to let the user search in upper case letters and still get the results
     const search = searchInput.value.toLowerCase();
     searchedList = [];
 
@@ -208,12 +128,17 @@ searchBar.addEventListener('keyup', (e) => {
             searchedList.push(student);
         }
     }
+
+    // clearing the old pagination links
     const unOrderedList = filterDiv.querySelector('ul');
     unOrderedList.remove();
+
+    // Initializing new pagination links that match the new search results
     createPagination(searchedList.length, 1);
     showPage(searchedList, 1);
 })
 
+
+// Initializing the page with the first 10 students and pagination links that match number of pages
 createPagination(students.length);
 showPage(students, 1);
-
